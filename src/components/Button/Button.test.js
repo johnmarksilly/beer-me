7267c62b2140c3712ecd5aa715c22f04d6a6f4b3
JohnMark Sill <johnmark.sill@infinitylabs.io>
2021-02-05 10:36:02 -0600

    Add button test

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,12 +1,20 @@
+import { render } from '@testing-library/react';
 import { shallow } from 'enzyme';
 import Button from './Button';
 
-const onClickSpy = jest.fn()
-
 test('Test button click', () => {
+  const onClickSpy = jest.fn()
   const wrapper = shallow(<Button onClick={onClickSpy} />)
-  const button = wrapper.find("button")
+  const button = wrapper.find('button')
   expect(onClickSpy).not.toHaveBeenCalled()
   button.simulate('click')
   expect(onClickSpy).toHaveBeenCalled()
 });
+
+test('Button color', () => {
+  let wrapper = render(<Button />)
+  expect(wrapper.container.firstChild.classList).not.toContain('primary')
+
+  wrapper = render(<Button color='primary' />)
+  expect(wrapper.container.firstChild.classList).toContain('primary')
+})
